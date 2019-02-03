073ec1e5745d582589bee63e06c3181bff610775
Quentin Burgniard <quentinburgniard@gmail.com>
2019-02-03 16:02:42 +0100

    fix(ci): git error

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,4 +3,4 @@ eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy.pem
 ssh-add .travis/deploy.pem
 git remote add deploy ssh://$CI_USER@$CI_HOST:$CI_DIRECTORY
-git push deploy
+git push deploy master:deploy
