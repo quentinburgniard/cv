6ad0085bedf780bb050b6a5475806fdec492623c
Quentin Burgniard <quentinburgniard@gmail.com>
2019-02-03 17:04:54 +0100

    fix(ci): ssh hostname

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,5 +2,5 @@
 eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy.pem
 ssh-add .travis/deploy.pem
-git remote add deploy ssh://$CI_USER@$CI_HOST:$CI_DIRECTORY
+git remote add deploy ssh://$CI_USER@$CI_HOST/~/$CI_DIRECTORY
 git push deploy master:deploy
