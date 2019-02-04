f1b5acc55b8d3cbd8b9f09277d9b15644dd4564e
Quentin Burgniard <quentinburgniard@gmail.com>
2019-02-04 11:03:53 +0100

    docs(ci): add link to use travis ci
    [skip ci]

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# https://docs.travis-ci.com/user/deployment/custom/#git
 eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy.pem
 ssh-add .travis/deploy.pem
-git fetch --unshallow
+
+# https://github.com/dwyl/learn-devops/issues/33
+git fetch --unshallow 
+
 git remote add deploy ssh://$CI_USER@$CI_HOST/~/$CI_DIRECTORY
 git push deploy master:deploy
