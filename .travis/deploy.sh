beadb31dae6adf27fbea1254b6af5efed9b66070
Quentin Burgniard <quentinburgniard@gmail.com>
2019-02-04 10:44:47 +0100

    fix(ci): git error
    https://github.com/dwyl/learn-devops/issues/33

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,5 +2,6 @@
 eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy.pem
 ssh-add .travis/deploy.pem
+git fetch --unshallow
 git remote add deploy ssh://$CI_USER@$CI_HOST/~/$CI_DIRECTORY
 git push deploy master:deploy
