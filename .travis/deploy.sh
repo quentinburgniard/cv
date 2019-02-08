340707490e5a8572fcec1e755d876c0154036cbf
Quentin Burgniard <quentinburgniard@gmail.com>
2019-02-08 16:04:55 +0100

    feat(ci): delete ci

--- .travis/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# https://docs.travis-ci.com/user/deployment/custom/#git
-eval "$(ssh-agent -s)"
-chmod 600 .travis/deploy.pem
-ssh-add .travis/deploy.pem
-echo $CI_DIRECTORY
-ssh -t $CI_USER@$CI_HOST "git reset --hard ${CI_DIRECTORY}"
-ssh -t $CI_USER@$CI_HOST "git pull ${CI_DIRECTORY}"
