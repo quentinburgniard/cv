1b9f2fad804f36bee215a567613eefcbec22c530
Quentin Burgniard <quentinburgniard@gmail.com>
2019-02-08 15:35:51 +0100

    fix(ci): git repository

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,5 +3,6 @@
 eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy.pem
 ssh-add .travis/deploy.pem
+echo $CI_DIRECTORY
 ssh -t $CI_USER@$CI_HOST "git reset --hard ${CI_DIRECTORY}"
 ssh -t $CI_USER@$CI_HOST "git pull ${CI_DIRECTORY}"
